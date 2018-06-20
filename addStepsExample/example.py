b61440ea5c50f6263aab3ad0ef62378866658847
Rémy Van der Zijden <remy_v.d.zijden@hotmail.fr>
2018-06-20 12:32:12 +0200

    Update example.py

--- addStepsExample/example.py
+++ addStepsExample/example.py
@@ -6,7 +6,6 @@ from PySide2.QtWidgets import *
 from addStepsDialogUi import Ui_addStepsDialog
 
 CSTM_STEP_WIDGET_HEIGHT = 20
-INDENTATION = 15
 
 PRESETS = [["step1", "step1.1"], ["step2", "step2.1", "step2.2"], ["step3"], ["step4", "step4.1", "step4.2"]]
 
@@ -28,9 +27,7 @@ class CustomStepWidget(QWidget):
 		button.setFixedHeight(CSTM_STEP_WIDGET_HEIGHT - 2)
 		button.setMaximumWidth(90)
 		button.clicked.connect(self.emitClick)
-		layout.addSpacing(INDENTATION)
 		layout.addWidget(button)
-		self.setStyleSheet("QPushButton {background-color: rgb(90,90,90); color: rgb(150,220,255);}")
 
 	def emitClick(self):
 		self.click.emit(self.parentIndex)
@@ -46,27 +43,17 @@ class AddStepsDialog(QDialog, Ui_addStepsDialog):
 		self.postSetup()
 
 	def postSetup(self):
-		self.addStepsTW.itemChanged.connect(self.changeItem)
 		self.addStepsTW.expandAll()
-		self.addStepsTW.setIndentation(INDENTATION)
-		self.addStepsTW.setFocusPolicy(Qt.NoFocus)
 		self.setWindowTitle("Manage steps")
-		self.setStyleSheet("QTreeView::indicator {background-color: rgb(90,90,90); color: rgb(230,230,230); border: none;}\
-							QTreeView::indicator:checked {image: url("+osp.join(osp.dirname(__file__), "check.png")+");}\
-							QTreeView {background-color: rgb(100,100,100);}")
 	
 	def addPresets(self):
 		for step in PRESETS:
 			newTop = QTreeWidgetItem([step[0]])
-			newTop.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable)
-			newTop.setCheckState(0, Qt.Unchecked)
-			newTop.setForeground(0, QBrush(QColor(220,220,220,255)))
+			newTop.setFlags(Qt.ItemIsEnabled)
 			self.addStepsTW.addTopLevelItem(newTop)
 			for i in range(1,len(step)):
 				newChild = QTreeWidgetItem([step[i]])
-				newChild.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable)
-				newChild.setCheckState(0, Qt.Unchecked)
-				newChild.setForeground(0, QBrush(QColor(220,220,220,255)))
+				newChild.setFlags(Qt.ItemIsEnabled)
 				newTop.addChild(newChild)
 
 	def addButtons(self):
@@ -76,27 +63,26 @@ class AddStepsDialog(QDialog, Ui_addStepsDialog):
 			cstmSubItem = QTreeWidgetItem()
 			cstmSubItem.setSizeHint(0, QSize(0, CSTM_STEP_WIDGET_HEIGHT))
 			cstmSubItem.setFlags(Qt.ItemIsEnabled)
-			cstmSubBtn = CustomStepWidget(self.addStepsTW, i)
-			cstmSubBtn.click.connect(self.addCustomStep)
+			cstmSubWidget = CustomStepWidget(self.addStepsTW, i)
+			cstmSubWidget.click.connect(self.addCustomStep)
 			step.addChild(cstmSubItem)
-			self.addStepsTW.setItemWidget(cstmSubItem, 0, cstmSubBtn)
+			self.addStepsTW.setItemWidget(cstmSubItem, 0, cstmSubWidget)
 		cstmItem = QTreeWidgetItem()
 		cstmItem.setSizeHint(0, QSize(0, CSTM_STEP_WIDGET_HEIGHT))
 		cstmItem.setFlags(Qt.ItemIsEnabled)
-		cstmBtn = CustomStepWidget(self.addStepsTW)
-		cstmBtn.click.connect(self.addCustomStep)
+		cstmWidget = CustomStepWidget(self.addStepsTW)
+		cstmWidget.click.connect(self.addCustomStep)
 		self.addStepsTW.addTopLevelItem(cstmItem)
-		self.addStepsTW.setItemWidget(cstmItem, 0, cstmBtn)
+		self.addStepsTW.setItemWidget(cstmItem, 0, cstmWidget)
 
 	@Slot(int)
 	def addCustomStep(self, parentIndex):
 		if self.addStepsTW.state() == QAbstractItemView.EditingState:
 			return
 		step = QTreeWidgetItem()
-		step.setCheckState(0, Qt.Checked)
-		step.setForeground(0, QBrush(QColor(150,220,255,255)))
-		step.setFlags(Qt.ItemIsEnabled | Qt.ItemIsUserCheckable | Qt.ItemIsEditable)
+		step.setFlags(Qt.ItemIsEnabled | Qt.ItemIsEditable)
 		if parentIndex == -1:
+			# no parent --> add top level item and add a child button to it
 			index = self.addStepsTW.invisibleRootItem().childCount() - 1
 			self.addStepsTW.insertTopLevelItem(index, step)
 			child = QTreeWidgetItem()
@@ -107,22 +93,12 @@ class AddStepsDialog(QDialog, Ui_addStepsDialog):
 			step.addChild(child) #<-- ISN'T THIS SUPPOSED TO WORK??
 			self.addStepsTW.setItemWidget(child, 0, cstmWidget)
 		else:
+			# parent exists --> add child step to it
 			parentItem = self.addStepsTW.invisibleRootItem().child(parentIndex)
 			parentItem.insertChild(parentItem.childCount() - 1, step)
 		self.addStepsTW.editItem(step, 0)
 
-	@Slot(QTreeWidgetItem, int)
-	def changeItem(self, item, column):
-		parent = item.parent()
-		if item.checkState(0) == Qt.Unchecked:
-			item.setForeground(0, QBrush(QColor(220,220,220,255)))
-			if not parent:
-				for i in range(item.childCount() - 1):
-					item.child(i).setCheckState(0, Qt.Unchecked)
-		else:
-			if parent:
-				parent.setCheckState(0, Qt.Checked)
-			item.setForeground(0, QBrush(QColor(150,220,255,255)))
+
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
